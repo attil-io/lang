5af4420280f2a4a51658b709fb1216dcfc969625
Attilio <parrotchaffinch@gmail.com>
2017-01-26 04:08:39 +0100

    implementation of inputstream_peek

--- src/lang/core.clj
+++ src/lang/core.clj
@@ -16,4 +16,8 @@
 		nextline (if isnewline (inc line) line)]
 		[ch (assoc inputstream_state :pos nextpos :col nextcol :line nextline)]))
 
-(defn inputstream_peek [inputstream_state])
+(defn inputstream_peek [inputstream_state]
+	(let [{:keys [pos input]} inputstream_state 
+		ch (if (> (count input) pos) (.charAt input pos) nil)]
+		ch))
+
--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -8,4 +8,5 @@
     (is (= [\a {:pos 1 :input "a" :line 0 :col 1}] (inputstream_next {:pos 0 :input "a" :line 0 :col 0})))
     (is (= [\newline {:pos 1 :input "\n" :line 1 :col 0}] (inputstream_next {:pos 0 :input "\n" :line 0 :col 0}))))
   (testing "test inputstream_peek"
-    (is (= nil (inputstream_peek {:pos 0 :input "" :line 0 :col 0})))))
+    (is (= nil (inputstream_peek {:pos 0 :input "" :line 0 :col 0})))
+    (is (= \a (inputstream_peek {:pos 0 :input "a" :line 0 :col 0})))))
