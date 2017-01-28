7312f0ee3147d0c7f3871f74918be5bdf16c7743
Attilio <parrotchaffinch@gmail.com>
2017-01-28 18:04:05 +0100

    read_string something

--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -115,7 +115,8 @@
     (is (= ["somet'hing" {:pos 11 :input "somet\\'hing" :line 0 :col 11}] (read_escaped {:pos 0 :input "somet\\'hing" :line 0 :col 0} \'))))
    (testing "test read_string"
     (is (= [{:type "str" :value "before"} {:pos 7 :input "before\"after" :line 0 :col 7}] (read_string {:pos 0 :input "before\"after" :line 0 :col 0})))
-    (is (= [{:type "str" :value ""} {:pos 1 :input "\"after" :line 0 :col 1}] (read_string {:pos 0 :input "\"after" :line 0 :col 0})))))
+    (is (= [{:type "str" :value ""} {:pos 1 :input "\"after" :line 0 :col 1}] (read_string {:pos 0 :input "\"after" :line 0 :col 0})))
+    (is (= [{:type "str" :value "something"} {:pos 9 :input "something" :line 0 :col 9}] (read_string {:pos 0 :input "something" :line 0 :col 0})))))
 
 
  
