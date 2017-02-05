52cc7a0bce2256aef50046ddb8e0a727e25dea3b
Attilio <parrotchaffinch@gmail.com>
2017-02-05 09:17:07 +0100

    Adapt tests for parse_skip_kw.

--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -167,12 +167,12 @@
   (testing "test parse_skip_punc"
     (is (= nil (parse_skip_punc nil nil)))
     (is (= {:pos 1 :input "," :line 0 :col 1} (parse_skip_punc \, {:pos 0 :input "," :line 0 :col 0})))
-    (is (thrown-with-msg? Exception #"Expecting punctuation: \"q\"" (parse_skip_punc \q, {:pos 0 :input "q + 1" :line 0 :col 0})))))
-(comment 
+    (is (thrown-with-msg? Exception #"Expecting punctuation: \"q\"" (parse_skip_punc \q, {:pos 0 :input "q + 1" :line 0 :col 0}))))
   (testing "test parse_skip_kw"
     (is (= nil (parse_skip_kw nil nil)))
-    (is (= [{:type "kw" :value "else"} {:pos 4 :input "else" :line 0 :col 4}] (parse_skip_kw "else" [{:pos 0 :input "else" :line 0 :col 0}])))
-    (is (thrown-with-msg? Exception #"Expecting keyword: \"hello\"" (parse_skip_kw "hello", [{:pos 0 :input "hello world" :line 0 :col 0}]))))
+    (is (= {:pos 4 :input "else" :line 0 :col 4} (parse_skip_kw "else" {:pos 0 :input "else" :line 0 :col 0})))
+    (is (thrown-with-msg? Exception #"Expecting keyword: \"hello\"" (parse_skip_kw "hello", {:pos 0 :input "hello world" :line 0 :col 0})))))
+(comment 
   (testing "test parse_skip_op"
     (is (= nil (parse_skip_op nil nil)))
     (is (= [{:type "op" :value "+"} {:pos 1 :input "+ 5" :line 0 :col 1}] (parse_skip_op "+" [{:pos 0 :input "+ 5" :line 0 :col 0}]))))
