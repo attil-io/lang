c069bf6e32ae68dc59ff2880f0f4bb8849e9d8b4
Attilio <parrotchaffinch@gmail.com>
2017-02-11 19:30:37 +0100

    mark place where parse_unexpected should be thrown

--- src/lang/core.clj
+++ src/lang/core.clj
@@ -302,7 +302,7 @@
 	; FIXME: if
 	(or (parse_is_kw "true" token_stream_state) (parse_is_kw "false" token_stream_state)) (parse_parse_bool token_stream_state)
 	(parse_is_kw "lambda" token_stream_state) (parse_parse_lambda (tokenstream_next token_stream_state))
-	:else (tokenstream_read_next token_stream_state)))
+	:else (tokenstream_read_next token_stream_state))) ; FIXME: parse_unexpected
 
 (defn parse_parse_expression [token_stream_state] 
 	(let [[parse_atom_value parse_atom_state] (parse_parse_atom token_stream_state)]
