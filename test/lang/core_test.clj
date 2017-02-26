4405bee85b56a5563d8d548e63a42ee0c7f65c30
Attilio <parrotchaffinch@gmail.com>
2017-02-26 20:50:21 +0100

    evaluate_apply_op "%" 2 3

--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -311,6 +311,7 @@
     (is (= 5 (evaluate_apply_op "+" 2 3)))
     (is (= 6 (evaluate_apply_op "*" 2 3)))
     (is (= -1 (evaluate_apply_op "-" 2 3)))
-    (is (= 2/3 (evaluate_apply_op "/" 2 3)))))
+    (is (= 2/3 (evaluate_apply_op "/" 2 3)))
+    (is (= 2 (evaluate_apply_op "%" 2 3)))))
  
 
