dcab32270a34187f4b0029e62179b0fb354d509d
Attilio <parrotchaffinch@gmail.com>
2017-02-26 23:13:24 +0100

    evaluate_apply_op ">=" "alma" 3

--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -329,6 +329,7 @@
     (is (= true (evaluate_apply_op "<=" 1 2)))
     (is (thrown-with-msg? Exception #"Expected number but got" (evaluate_apply_op "<=" "alma" 3)))
     (is (= false (evaluate_apply_op ">=" 1 2)))
+    (is (thrown-with-msg? Exception #"Expected number but got" (evaluate_apply_op ">=" "alma" 3)))
     (is (= false (evaluate_apply_op "==" 1 2)))
     (is (= true (evaluate_apply_op "!=" 1 2)))
     (is (thrown-with-msg? Exception #"Can't apply operator" (evaluate_apply_op "<>" 1 2)))))
