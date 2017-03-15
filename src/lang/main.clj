d6a8b936b61c4228b06310797320713988c8a7e1
Attilio <parrotchaffinch@gmail.com>
2017-03-15 10:36:15 +0100

    Call the interpreter for each line of input.

--- src/lang/main.clj
+++ src/lang/main.clj
@@ -1,12 +1,16 @@
 (ns lang.main
+	(:require [lang.interpret :refer :all])
 	(:gen-class))
 
 (defn -main [& args]
 	(do 
 	(println "lang REPL, arguments: " args)
+	(print "> ") (flush)
 	(loop [nextinput (read-line)]
 	(if (= "exit" nextinput) nil
             (do 
-		(println (str "You typed: " nextinput))
+		(let [[result output] (interpret nextinput)]
+			(println (str (clojure.string/join " " output) "\n\n >> " result)))
+		(print "> ") (flush)
 		(recur (read-line)))))))
 
