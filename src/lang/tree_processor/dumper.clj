3237957f6285f4a742cc19e5710a7fca0993a981
Attilio <parrotchaffinch@gmail.com>
2018-03-28 00:28:51 +0200

    Walking tree using protocol containing callbacks

--- src/lang/tree_processor/dumper.clj
+++ src/lang/tree_processor/dumper.clj
@@ -2,43 +2,54 @@
         (:require [lang.parser :refer :all])
         (:require [clojure.string :as s :only (join)]))
 
-(declare dump-prog)
-(declare dump-var)
-(declare dump-binary)
-(declare dump-assign)
-(declare dump-lambda)
-(declare dump-let)
-(declare dump-if)
-(declare dump-call)
-(declare dump-tree)
-
 (def ^{:private true} FALSE { :type "bool" :value false })
+(declare walk-tree)
 
-(defn dump [code] (dump-tree (parse_parse_toplevel code)))
+(defprotocol WALKER-CALLBACK
+    (on-prog [this ast])
+    (on-num [this ast])
+    (on-str [this ast])
+    (on-bool [this ast])
+    (on-var [this ast])
+    (on-binary [this ast])
+    (on-assign [this ast])
+    (on-lambda [this ast])
+    (on-let [this ast])
+    (on-if [this ast])
+    (on-call [this ast]))
 
-(defn dump-tree [ast] (let [ast_type (:type ast)]
-        (case ast_type 
+
+(defn- internal-let-var [this v] (str (:name v) "=" (walk-tree (:def v) this)))
+
+(deftype dump-callback [] WALKER-CALLBACK
+    (on-prog [this ast] (str "{" (s/join \newline (map #(walk-tree % this) (:prog ast))) "}"))
+    (on-num [this ast] (str (:value ast)))
+    (on-str [this ast] (str (:value ast)))
+    (on-bool [this ast] (str (:value ast)))
+    (on-var [this ast] (str (:value ast)))
+    (on-binary [this ast] (str (walk-tree (:left ast) this) (:operator ast) (walk-tree (:right ast) this)))
+    (on-assign [this ast] (on-binary this ast))
+    (on-lambda [this ast] (str "function " (:name ast) "(" (s/join "," (map str (:vars ast))) "){return " (walk-tree (:body ast) this) "}"))
+    (on-let [this ast] (str "let (" (s/join "," (map #(internal-let-var this %) (:vars ast))) "){" (walk-tree (:body ast) this) "}"))
+    (on-if [this ast] (str "if (" (walk-tree (:cond ast) this) "){" (walk-tree (:then ast) this) "}{" (walk-tree (or (:else ast) FALSE) this) "}"))
+    (on-call [this ast] (str (walk-tree (:func ast) this) "(" (s/join "," (map #(walk-tree % this) (:args ast))) ")")))
+
+
+(defn walk-tree ([ast cb]
+        (case (:type ast)
               nil nil
-              "prog" (dump-prog ast)
-              "num" (str (:value ast))
-              "str" (str (:value ast))
-              "bool" (str (:value ast))
-              "var" (dump-var (:value ast))
-              "binary" (dump-binary ast)
-              "assign" (dump-assign ast)
-              "lambda" (dump-lambda ast)
-              "let" (dump-let ast)
-              "if" (dump-if ast)
-              "call" (dump-call ast)
-              )))
+              "prog" (on-prog cb ast)
+              "num" (on-num cb ast)
+              "str" (on-str cb ast)
+              "bool" (on-bool cb ast)
+              "var" (on-var cb ast)
+              "binary" (on-binary cb ast)
+              "assign" (on-assign cb ast)
+              "lambda" (on-lambda cb ast)
+              "let" (on-let cb ast)
+              "if" (on-if cb ast)
+              "call" (on-call cb ast)))
+  ([ast] (walk-tree ast (dump-callback.))))
 
-(defn- dump-var [value] (str value))
-(defn- dump-binary [ast] (str (dump-tree (:left ast)) (:operator ast) (dump-tree (:right ast))))
-(defn- dump-assign [ast] (dump-binary ast))
-(defn- dump-lambda [ast] (str "function " (:name ast) "(" (s/join "," (map dump-var (:vars ast))) "){return " (dump-tree (:body ast)) "}"))
-(defn- dump-let-var [v] (str (dump-var (:name v)) "=" (dump-tree (:def v))))
-(defn- dump-let [ast] (str "let (" (s/join "," (map dump-let-var (:vars ast))) "){" (dump-tree (:body ast)) "}"))
-(defn- dump-if [ast] (str "if (" (dump-tree (:cond ast)) "){" (dump-tree (:then ast)) "}{" (dump-tree (or (:else ast) FALSE) ) "}"))
-(defn- dump-call [ast] (str (dump-tree (:func ast)) "(" (s/join "," (map dump-tree (:args ast))) ")"  ))
-(defn- dump-prog [ast] (str "{" (s/join \newline (map dump-tree (:prog ast))) "}"))
+(defn dump [code] (walk-tree (parse_parse_toplevel code) (dump-callback.)))
 
